81edd8a9728257ba9f0539ac34a2d344ffcbe75c
Icaro Oliveira <icaro.rodrigo@gmail.com>
2023-07-17 18:44:53 -0300

    [feat] desenvolvimento da Entidade Account

--- src/main/java/br/com/ebanx/home/test/entity/AccountEntity.java
+++ src/main/java/br/com/ebanx/home/test/entity/AccountEntity.java
@@ -1,4 +1,23 @@
 package br.com.ebanx.home.test.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Table(name = "account")
+@Entity(name = "AccountEntity")
+@Getter
+@Setter
 public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(nullable = false)
+    private Integer accountCode;
+    @Column(nullable = false)
+    private BigDecimal amount;
 }
