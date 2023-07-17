820f8c5aa10fd1b6aa61bfc71efb7d0ff1f8ac44
Icaro Oliveira <icaro.rodrigo@gmail.com>
2023-07-17 18:49:10 -0300

    [fix] Correção da definição do repositório de classe para interface

--- src/main/java/br/com/ebanx/home/test/repository/AccountRepository.java
+++ src/main/java/br/com/ebanx/home/test/repository/AccountRepository.java
@@ -1,4 +1,9 @@
 package br.com.ebanx.home.test.repository;
 
-public class AccountRepository {
+import br.com.ebanx.home.test.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
 }
